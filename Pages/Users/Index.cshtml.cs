042b4cd4af49716a4656d03944772139354f5ba5
Luis Miguel Ossa Arias <1006016025@u.icesi.edu.co>
2022-02-04 19:08:12 -0500

    the css style for the login an the sign up has been added

--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -19,7 +19,7 @@ namespace RPC.Pages_Users
             _context = context;
         }
 
-        public IList<User> User { get; set; }
+        public IList<User> User { get;set; }
 
         public async Task OnGetAsync()
         {
