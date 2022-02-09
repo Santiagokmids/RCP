c2fa5ac54ed4045a72f008dbcf43d4f0056cb1e2
SANTIAGO TROCHEZ VELASCO <1193573597@u.icesi.edu.co>
2022-02-09 11:29:31 -0500

    Program already

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RPC.Pages_Users;
+using RPC.Models;
 
 namespace RPC.Pages
 {
@@ -10,14 +11,34 @@ namespace RPC.Pages
 
         private readonly RazorPagesUserContext _context;
 
+        public bool isLogged;
+
         public IndexModel(ILogger<IndexModel> logger, RazorPagesUserContext context)
         {
             _logger = logger;
             _context = context;
+            isLogged = true;
         }
         public void OnGet()
         {
         }
 
+        [BindProperty]
+        public User UserRPC { get; set; }
+
+        public void OnPost()
+        {
+            var dataBaseUser = _context.User.FirstOrDefault(u => u.UserName == UserRPC.UserName && u.Password == UserRPC.Password);
+
+            if (dataBaseUser != null)
+            {
+                Response.Redirect("/Users/Main");
+            }
+            else
+            {
+                isLogged = false;
+                Page();
+            }
+        }
     }
 }
